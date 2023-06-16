d86e9e49ecc595d1fa70deb2931e88cb730a9826
Eszter Pitas <eszterpitas@gmail.com>
2023-06-16 16:14:11 +0200

    drawMap with for

--- minesweeper.js
+++ minesweeper.js
@@ -6,11 +6,18 @@ const size = 50;
 let x = 0;
 let y = 0;
 const columns = canvas.width / size;
-
-for (let i = 0; i < columns; i++) {
-  drawImage(i * size, 0);
-}
+const rows = canvas.height / size;
 
 function drawImage(x, y) {
   c.drawImage(image, x, y, size, size);
 }
+
+function drawMap() {
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < columns; i++) {
+      drawImage(i * size, j * size);
+    }
+  }
+}
+
+drawMap();
