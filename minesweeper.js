b51f6b9cda23c3e57fb6867631ed635743b986e1
Eszter Pitas <eszterpitas@gmail.com>
2023-06-20 14:39:23 +0200

    placing random mines

--- minesweeper.js
+++ minesweeper.js
@@ -3,32 +3,29 @@ const canvas = document.getElementById("myCanvas");
 const c = canvas.getContext("2d");
 
 const size = 50;
-let x = 0;
-let y = 0;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
-
 const mine = 'mine'
+const mineCount = 5;
 
-let map = createMap();  
-map[0][0] = mine;
-map[5][6] = mine;
+let map = createMap();
+placeMines(map, mineCount);
 
-console.log(map);
+console.log(map); 
 
-function drawImage(x, y) {
-  c.drawImage(image, x, y, size, size);
-}
+drawMap();
 
-function drawMap() {
-  for (let j = 0; j < rows; j++) {
-    for (let i = 0; i < columns; i++) {
-      drawImage(i * size, j * size);
+function placeMines(map, mineCount) {
+  let mines = 0;
+  while (mines < mineCount) {
+    let x = Math.floor(Math.random() * columns);
+    let y = Math.floor(Math.random() * rows);
+    if (map[y][x] !== mine) {
+      map[y][x] = mine;
+      mines++;
     }
   }
 }
-
-drawMap();
 
 function createMap() {
   let map = [];
@@ -42,3 +39,14 @@ function createMap() {
   return map;
 }
 
+function drawMap() {
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < columns; i++) {
+      drawImage(i * size, j * size);
+    }
+  }
+}
+
+function drawImage(x, y) {
+  c.drawImage(image, x, y, size, size);
+}
