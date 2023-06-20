1c217707255c1a8999deeb42b5085e7c5b70eca9
Eszter Pitas <eszterpitas@gmail.com>
2023-06-20 09:50:11 +0200

    arrays practice

--- minesweeper.js
+++ minesweeper.js
@@ -8,6 +8,14 @@ let y = 0;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
 
+let firstRow = [0, 0, 0, 9, 0, 0, 0, 0, 0, 0];
+firstRow [3] = 11;
+
+let first = firstRow[3];
+let second = firstRow[4];
+console.log(first, second);
+console.log(firstRow.length);
+
 function drawImage(x, y) {
   c.drawImage(image, x, y, size, size);
 }
